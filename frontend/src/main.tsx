fab59e740a13bad27656953685e271373b579a20
Shreshtthh <shreshth013@gmail.com>
2026-01-17 09:15:58 +0530

    Add Buffer polyfill for browser compatibility

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,3 +1,7 @@
+// Buffer polyfill for browser compatibility (casper-js-sdk uses Node.js Buffer)
+import { Buffer } from 'buffer'
+window.Buffer = Buffer
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
